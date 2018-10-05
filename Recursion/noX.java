bca1a5e2b6cddab9ec444a177c4d7bb43d7c1658
manojkumarsmks <manojkumarsmks@gmail.com>
2018-10-05 01:21:04 -0600

    noX

---
+++ Recursion/noX.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class noX {
+
+	public static void main(String[] args) {
+		System.out.println(noX("xx"));
+	}
+	
+	public static String noX(String str) {
+		  if(str.length() == 0)
+			  return "";
+		  if(str.charAt(0) == 'x')
+			  return noX(str.substring(1));
+		  else 
+			  return str.charAt(0) + noX(str.substring(1));
+	}
+}
