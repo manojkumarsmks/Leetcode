58aaf782b736513d8ef98a7f94ded30cc6da2bd5
manojkumarsmks <manojkumarsmks@gmail.com>
2018-10-06 14:33:19 -0600

    Nest Parentheses

---
+++ Recursion/NestParen.java
@@ -0,0 +1,19 @@
+// https://codingbat.com/prob/p183174
+package Recursion;
+
+public class NestParen {
+
+	public static void main(String[] args) {
+		System.out.println(nestParen("(((x))"));
+	}
+	
+	public static boolean nestParen(String str) {
+		
+		if(str.length() == 0)
+			return true;
+		if(str.charAt(0) == '(' && str.charAt(str.length()-1) == ')')
+			return true && nestParen(str.substring(1, str.length()-1));
+		else 
+			return false;
+	}
+}
