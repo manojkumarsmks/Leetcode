0905dab8e4d4c2cea2b549ac7b584a20ed9c363a
manojkumarsmks <manojkumarsmks@gmail.com>
2018-10-07 23:24:48 -0600

    Count Hi 2

---
+++ Recursion/CountHi2.java
@@ -0,0 +1,21 @@
+// https://codingbat.com/prob/p143900
+package Recursion;
+
+public class CountHi2 {
+
+	public static void main(String[] args) {
+		System.out.println(countHi2("hixhhi"));
+	}
+	
+	public static int countHi2(String str) {
+		if(str.length() <= 1)
+			return 0;
+		if(str.length() > 2 && str.substring(0,3).equals("xhi"))
+			return countHi2(str.substring(3));
+		if(str.substring(0, 2).equals("hi"))
+			return 1 + countHi2(str.substring(2));
+		else
+			return countHi2(str.substring(1));
+	}
+
+}
