dcebc9fccaf46487a94275613d4cd7ce461e20b6
manojkumarsmks <manojkumarsmks@gmail.com>
2018-10-10 01:14:11 -0600

    Solution with comparator

--- MeetingRooms.java
+++ MeetingRooms.java
@@ -1,12 +1,13 @@
 // 252 - Meeting Rooms
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class MeetingRooms {
 
 	public static void main(String[] args) {
 		
-		Interval[] intervalArray = {new Interval(7,10), new Interval(2,4)};
-		System.out.println(meetingRooms(intervalArray));
+		Interval[] intervalArray = { new Interval(0,30), new Interval(5,10), new Interval(15,20)};
+		System.out.println(meeting(intervalArray));
 		
 		
 	}
@@ -29,10 +30,28 @@ public class MeetingRooms {
 			if(start[i+1] < end[i])
 				return false;
 		}
+		return true;
+	}
+	
+	// With Comparator 
+	public static boolean meeting(Interval[] interval) {
+		Arrays.sort(interval, new Comparator<Interval>() {
+
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				return o1.start - o2.start;
+			}
+		});
+		
+		for (int i = 0; i <= interval.length-2; i++) {
+			if(interval[i+1].start < interval[i].end)
+				return false;
+		}
+		
 		
 		return true;
-		
 	}
+	
 
 }
 
