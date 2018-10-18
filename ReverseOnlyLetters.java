7732f9bb162fc3b7bb70977dce8bca8e5832cb95
manojkumarsmks <manojkumarsmks@gmail.com>
2018-10-17 22:14:28 -0600

    Pointer method

--- ReverseOnlyLetters.java
+++ ReverseOnlyLetters.java
@@ -1,12 +1,13 @@
+// https://leetcode.com/problems/reverse-only-letters/description/
 import java.util.Stack;
 
-// https://leetcode.com/problems/reverse-only-letters/description/
 public class ReverseOnlyLetters {
 
 	public static void main(String[] args) {
 		System.out.println(reverseOnlyLetters(";1yDV"));
 	}
 	
+	// Stack Based Solution
     public static String reverseOnlyLetters(String S) {
     	
     	if(S.length() <= 1)
@@ -29,6 +30,36 @@ public class ReverseOnlyLetters {
     	}
     	
     	return stringBuilder.toString();
+    }
+    
+    // Pointer based solution
+    public String reverseOnlyLetters2(String S) {
+        int i = 0, j = S.length()-1;
+        char[] SArray = S.toCharArray();
+        while(i < j) {
+            
+            if(Character.isAlphabetic(SArray[i]) && Character.isAlphabetic(SArray[j])) {
+                char temp = SArray[i];
+                SArray[i] = SArray[j];
+                SArray[j] = temp;
+                i++;
+                j--;
+            }
+           else if (Character.isAlphabetic(SArray[i]) && !Character.isAlphabetic(SArray[j])) {
+                j--;
+            }
+            else if (!Character.isAlphabetic(SArray[i]) && Character.isAlphabetic(SArray[j])) {
+                i++;
+            }
+            else {
+                i++;
+                j--;
+            }
+            
+        }
+        
+        return new String(SArray);
+        
     }
 
 }
