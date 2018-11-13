a00922ec391a7d4cb0feb9321ffa57178a9bbe0b
manojkumarsmks <manojkumarsmks@gmail.com>
2018-11-13 00:18:31 -0700

    Better solution

--- BuynSellStockI.java
+++ BuynSellStockI.java
@@ -4,7 +4,7 @@ public class BuynSellStockI {
 	public static void main(String[] args) {
 		int[] prices = {7,1,5,3,6,4};
 		
-		System.out.println(maxProfit(prices));
+		System.out.println(maxProf(prices));
 	}
 	
 	public static int maxProfit(int[] prices) {
@@ -16,6 +16,24 @@ public class BuynSellStockI {
 				max = Math.max(max,prices[j]-prices[i]);
 			}
 		}
+		
+		return max;
+	}
+	
+	
+	public static int maxProf(int[] prices)  {
+		
+		if(prices == null || prices.length == 0)
+			return 0;
+		
+		int min = prices[0];
+		int max = 0;
+		
+		for (int i = 1; i < prices.length; i++) {
+			min = Math.min(min, prices[i]);
+			max = Math.max(max, prices[i]-min);
+		}
+		
 		
 		return max;
 	}
