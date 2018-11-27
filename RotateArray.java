10293f60a2935afebc20e43307bbfbaa664de421
manojkumarsmks <manojkumarsmks@gmail.com>
2018-11-27 16:13:38 -0700

    Rotate array better methods

--- RotateArray.java
+++ RotateArray.java
@@ -1,13 +1,14 @@
-
+// https://leetcode.com/problems/rotate-array/
 public class RotateArray {
 
 	public static void main(String[] args) {
-		int[] nums = {-1,-100,3,99};
+		int[] nums = {1,2,3,4,5,6,7};
 		int k = 2;
 		
-		rotate(nums, k);
+		rotateC(nums, k);
 	}
 	
+	// Brute Force method
 	public static void rotate(int[] nums, int k) {
 		
 		if(nums.length < 2)
@@ -31,5 +32,41 @@ public class RotateArray {
 		for(Integer integer:nums)
 			System.out.println(integer);
 	}
-
+	
+	// Using another array
+	public static void rotateB(int[] nums, int k) {
+		int[] temp = new int[nums.length];
+		
+		for(int i = 0; i < nums.length; i++) {
+			temp[(i+k)%nums.length] = nums[i];
+		}
+		
+		for(int i = 0; i < nums.length; i++) {
+			nums[i] = temp[i];
+		}
+	}
+	
+	//Reversing the arrays
+	public static void rotateC(int[] nums, int k) {
+		if(nums.length < 2)
+            return;
+        if(k > nums.length)
+            k %= nums.length;
+        
+		reverse(nums, 0, nums.length-1);
+		reverse(nums, 0, k-1);
+		reverse(nums,k, nums.length-1);
+		for(Integer integer:nums)
+			System.out.println(integer);
+	}
+	
+	public static void reverse(int[] nums, int start, int end) {
+		while(start < end) {
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
+			start++;
+			end--;	
+		}
+	}
 }
