96a8f588a394ea816678ce311c91e909265d83f5
manojkumarsmks <manojkumarsmks@gmail.com>
2018-11-28 00:39:39 -0700

    Max of 3 products

---
+++ MaxOf3Products.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/maximum-product-of-three-numbers/
+public class MaxOf3Products {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4 };
+		System.out.println(maximumProduct(nums));
+	}
+
+	public static int maximumProduct(int[] nums) {
+
+		int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+		int max1 = Integer.MIN_VALUE, max2 = Integer.MAX_VALUE, max3 = Integer.MAX_VALUE;
+
+		for (int n : nums) {
+			if (n <= min1) {
+				min2 = min1;
+				min1 = n;
+			} else if (n <= min2) {
+				min2 = n;
+			}
+			if(n >= max1) {
+				max3 = max2;
+				max2 = max1;
+				max1 = n;
+			}
+			else if(n >= max2){
+				max3 = max2;
+				max2 = n;
+			}
+			else if(n > max3) {
+				max3 = n;
+			}
+		}
+		return Math.max((min1*min2*max1), (max1*max2*max3));
+	}
+}
