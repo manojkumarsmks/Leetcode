30704c31a5829a19ad6b350d9889ba25086dc27f
manojkumarsmks <manojkumarsmks@gmail.com>
2018-12-06 21:53:16 -0700

    Recursive Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,14 +2,14 @@
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		int [] array = {-1,0,3,5,9,12};
+		int [] array = {5};
 		
-		System.out.println(Search(array, array.length-1, 0, 2));
+		System.out.println(Search(array, array.length-1, 0, 5));
 	}
 
 	public static int Search(int[] array, int high, int low, int element) {
 		
-		if(high <= low)
+		if(high < low)
 			return -1;
 		
 		int mid = low +(high-low)/2;
